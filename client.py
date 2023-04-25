822e0060793b7b3d75f2ea4b56bf445d78ba0182
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-04-25 12:54:00 -0300

    Thread added

--- client.py
+++ client.py
@@ -1,11 +1,22 @@
 import socket
+from threading import Thread
 
 HOST = "127.0.0.1"
 PORT = 12345
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello server")
-    data = s.recv(1024)
 
-print(f"Received {data}")
+def receive_messages(client_socket):
+    while True:
+        message = client_socket.recv(1024)
+        print(f"Message received from the server: {message.decode()}")
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.connect((HOST, PORT))
+    print(f"Connected to server {HOST}:{PORT}")
+
+    Thread(target=receive_messages, args=(client_socket,)).start()
+
+    while True:
+        message = input("Enter your message: ")
+        client_socket.sendall(message.encode())
--- server.py
+++ server.py
@@ -1,22 +1,37 @@
 import socket
+from threading import Thread
 
 HOST = "127.0.0.1"
 PORT = 12345
 
 
-def new_client(connection: socket.socket) -> None:
-    with connection:
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                break
-            print(f"Received {data}")
-            connection.sendall(b"Hello client")
+def handle_client(client_socket, client_address):
+    print(f"Client connected {client_address}")
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
     while True:
-        conn, _ = s.accept()
-        new_client(conn)
+        message = client_socket.recv(1024)
+        if message:
+            print(f"Message received from client {client_address}: {message.decode()}")
+            broadcast(message, client_address)
+        else:
+            client_socket.close()
+            break
+
+
+def broadcast(message, sender_address):
+    for client in clients:
+        if client[1] != sender_address:
+            client[0].sendall(message)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print(f"The server is running on {HOST}:{PORT}")
+
+    clients = []
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        clients.append((client_socket, client_address))
+        Thread(target=handle_client, args=(client_socket, client_address)).start()
